0b88ded6156de7dae1449acf03cee2398be877bb
Pablo <pmiguelferrer@gmail.com>
2025-11-24 21:48:45 +0000

    Add bike and position data model

---
+++ fs-2025-assessment-1-80457/Models/Bike.cs
@@ -0,0 +1,19 @@
+﻿namespace fs_2025_assessment_1_80457.Models
+{
+    public class Bike
+    {
+        public int number { get; set; }
+        public string contract_name { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public Position position { get; set; }
+        public bool banking { get; set; }
+        public bool bonus { get; set; }
+        public int bike_stands { get; set; }
+        public int available_bike_stands { get; set; }
+        public int available_bikes { get; set; }
+        public string status { get; set; }
+        public object last_update { get; set; }
+    }
+
+}
